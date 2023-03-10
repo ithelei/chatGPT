26609c199b4ab8771b82dc9aaff06fcb0ebefa97
helei <helei>
2023-03-10 17:48:50 +0800

    删掉技术支持

--- src/default.ts
+++ src/default.ts
@@ -8,6 +8,6 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 - 本站仅用于演示，填入自己的 key 才可使用。
-- 联系v：A_309309
+- OpenAI API Key : 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
 - 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 提供支持。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
